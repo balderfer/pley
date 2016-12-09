f1ef8c53725f3c663b0e164f6a00eb10e272dbcc
Rhys Howell <Anemy@users.noreply.github.com>
2016-12-09 13:31:10 -0500

    Update layout

--- src/client/components/layout.jsx
+++ src/client/components/layout.jsx
@@ -29,7 +29,7 @@ class Layout extends React.Component {
               </Link>
               <Link to="/login">
                 <li>
-                    <button className="signInButton">Sign in</button>
+                    <button">Sign in</button>
                 </li>
               </Link>
             </ul>
