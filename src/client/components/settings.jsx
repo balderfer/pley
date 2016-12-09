8eb06be5b9fab1c404d6e34b8e673d4bfa5be7c9
Ben Alderfer <ben.alderfer@gmail.com>
2016-12-09 01:57:00 -0500

    Fixed settings

--- src/client/components/settings.jsx
+++ src/client/components/settings.jsx
@@ -146,84 +146,82 @@ class Settings extends React.Component {
   render() {
     return (
       <DashboardPage>
-        <div className='about'>
-          <div className="settings-container">
-            <div className="text-container  form">
-              <div className="input-row">
-                <h2>Settings</h2>
-              </div>
-
-              <div className="input-row">
-                <label htmlFor="name">Name</label>
-                <input type="text"
-                  id="name"
-                  className="nameInput"
-                  placeholder="Name"
-                  onChange={e => {
-                    this.setName(e.target.value);
-                  }}
-                  value={this.state.name}
-                />
-              </div>
-              
-              <div className="input-row">
-                <label htmlFor="currentPassword">Current Password</label>
-                <input type="password"
-                  id="currentPassword"
-                  className="passwordInput"
-                  placeholder="Current Password"
-                  onChange={e => {
-                    this.setPassword(e.target.value);
-                  }}
-                  value={this.state.password}
-                />
-              </div>
-
-              <div className="input-row">
-                <label htmlFor="newPassword">New Password <span className="smallSettingsLabel">(Leave this blank if you don{'\''}t want to change it.)</span></label>
-                <input type="password"
-                  id="newPassword"
-                  className="passwordInput"
-                  placeholder="New Password"
-                  minLength="6" 
-                  maxLength="24"
-                  onChange={e => {
-                    this.setNewPassword(e.target.value);
-                  }}
-                  value={this.state.newPassword}
-                />
-              </div>
-              
-              <div className="input-row">
-                <label htmlFor="newPasswordConfirm">Confirm new password</label>
-                <input type="password"
-                  id="newPasswordConfirm"
-                  className="passwordInput"
-                  placeholder="Re-Enter New Password"
-                  minLength="6" 
-                  maxLength="24"
-                  onChange={e => {
-                    this.setConfirmPassword(e.target.value);
-                  }}
-                  value={this.state.confirmPassword}
-                />
-              </div>
-              
-              <div className="input-row">
-                <button
-                  className="saveSettings"
-                  onClick={() => {
-                    if(!this.state.inSaveRequest) {
-                      this.saveSettings();
-                    }
-                  }}>
-                  Save
-                </button>
-              </div>
-              
-              {this.renderSuccessfulSaveAttemptMessages()}
-              {this.renderUnsuccessfulSaveAttemptMessages()}
+        <div className="settings-container">
+          <div className="text-container  form">
+            <div className="input-row">
+              <h2>Settings</h2>
             </div>
+
+            <div className="input-row">
+              <label htmlFor="name">Name</label>
+              <input type="text"
+                id="name"
+                className="nameInput"
+                placeholder="Name"
+                onChange={e => {
+                  this.setName(e.target.value);
+                }}
+                value={this.state.name}
+              />
+            </div>
+            
+            <div className="input-row">
+              <label htmlFor="currentPassword">Current Password</label>
+              <input type="password"
+                id="currentPassword"
+                className="passwordInput"
+                placeholder="Current Password"
+                onChange={e => {
+                  this.setPassword(e.target.value);
+                }}
+                value={this.state.password}
+              />
+            </div>
+
+            <div className="input-row">
+              <label htmlFor="newPassword">New Password <span className="smallSettingsLabel">(Leave this blank if you don{'\''}t want to change it.)</span></label>
+              <input type="password"
+                id="newPassword"
+                className="passwordInput"
+                placeholder="New Password"
+                minLength="6" 
+                maxLength="24"
+                onChange={e => {
+                  this.setNewPassword(e.target.value);
+                }}
+                value={this.state.newPassword}
+              />
+            </div>
+            
+            <div className="input-row">
+              <label htmlFor="newPasswordConfirm">Confirm new password</label>
+              <input type="password"
+                id="newPasswordConfirm"
+                className="passwordInput"
+                placeholder="Re-Enter New Password"
+                minLength="6" 
+                maxLength="24"
+                onChange={e => {
+                  this.setConfirmPassword(e.target.value);
+                }}
+                value={this.state.confirmPassword}
+              />
+            </div>
+            
+            <div className="input-row">
+              <button
+                className="saveSettings"
+                onClick={() => {
+                  if(!this.state.inSaveRequest) {
+                    this.saveSettings();
+                  }
+                }}>
+                Save
+              </button>
+            </div>
+            
+            {this.renderSuccessfulSaveAttemptMessages()}
+            {this.renderUnsuccessfulSaveAttemptMessages()}
           </div>
         </div>
       </DashboardPage>
